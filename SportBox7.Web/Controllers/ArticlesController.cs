8894464d552649eb34cdfd44e65beec91af35e98
Lyubomir Kavdanski <kavdansky@gmail.com>
2022-01-08 23:34:29 +0200

    Split Articles Controller
    Remove Edit and Create Articles  to EditorsArticlesController

--- SportBox7.Web/Controllers/ArticlesController.cs
+++ SportBox7.Web/Controllers/ArticlesController.cs
@@ -17,16 +17,7 @@
 
     public class ArticlesController: MainController
     {
-        private readonly IArticleRepository articleRepository;
-        private readonly ISourceRepository sourceRepository;
-        private readonly IMapper mapper;
 
-        public ArticlesController(IArticleRepository articleRepository, ISourceRepository sourceRepository, IMapper mapper)
-        {
-            this.articleRepository = articleRepository;
-            this.sourceRepository = sourceRepository;
-            this.mapper = mapper;
-        }
         [HttpGet]
         [Route("/articles")]
         public async Task<ActionResult<ListArticlesByCategoryOutputModel>> Category([FromQuery] ListArticlesByCategoryQuery query)
@@ -39,39 +30,7 @@
             return Redirect("/Home/NotFound");
         }
 
-        [Route("/articles/edit")]
-        [HttpGet]
-        [Authorize]
-        public async Task<ActionResult<EditArticleOutputModel>> Edit([FromQuery]EditArticleOutputModelQuery query)
-        {
-            try
-            {
-                var model = await this.Mediator.Send(query);
-                model.Categories = await articleRepository.GetMenuCategories();
-                model.Sources = await sourceRepository.GetSources();
-                return View(model);
-            }
-            catch (NullReferenceException)
-            {
-                return Redirect("/Home/NotFound");
-            }
-        }
-
-        [Route("/articles/edit")]
-        [HttpPost]
-        [Authorize]
-        public async Task<ActionResult<EditArticleOutputModel>> Edit(EditArticleCommand command)
-        {
-            try
-            {
-                var model = await this.Mediator.Send(command);
-                return model;
-            }
-            catch (ModelValidationException ex)
-            {
-                return RedirectToAction("Edit", new EditArticleOutputModelQuery() {Id = command.Id, Errors = ex.Errors });
-            }
-        }
+       
 
         [Route("/articles/all")]
         [HttpGet]
@@ -83,39 +42,6 @@
                 return View(model);
             }
             return Redirect("/Home/NotFound"); 
-        }
-
-        [Route("/articles/create")]
-        [HttpGet]
-        [Authorize]
-        public async Task<ActionResult<CreateDraftArticleOutputModel>> Create(CreateDraftArticleOutputModel model)
-        {
-            if (model.Errors.Count == 0)
-            {
-                return View(await CreateDraftArticleOutputModel.CreateAsync(articleRepository, sourceRepository));
-            }
-            model.Categories = await articleRepository.GetMenuCategories();
-            model.Sources = await sourceRepository.GetSources();
-            return View(model);
-        }
-       
-        [HttpPost]
-        [Authorize]
-        public async Task<ActionResult<CreateArticleOutputModel>> Create(CreateArticleCommand command)
-        {
-            try
-            {
-                var result = await this.Send(command);
-                return result;
-            }
-            catch (ModelValidationException ex)
-            {
-                command.Errors = ex.Errors;
-                return RedirectToAction("Create", command);
-            }
-            
-        } 
-
-         
+        }     
     }
 }
---
+++ SportBox7.Web/Controllers/EditorsArticlesController.cs
@@ -0,0 +1,103 @@
+﻿namespace SportBox7.Web.Controllers
+{
+    using AutoMapper;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using SportBox7.Application.Exceptions;
+    using SportBox7.Application.Features.Articles.Commands.Create;
+    using SportBox7.Application.Features.Articles.Commands.Edit;
+    using SportBox7.Application.Features.Articles.Contrcts;
+    using SportBox7.Application.Features.Articles.Queries.Create;
+    using SportBox7.Application.Features.Articles.Queries.Edit;
+    using SportBox7.Application.Features.Sources;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class EditorsArticlesController : MainController
+    {
+        private readonly IArticleRepository articleRepository;
+        private readonly ISourceRepository sourceRepository;
+        private readonly IMapper mapper;
+
+        public EditorsArticlesController(IArticleRepository articleRepository, ISourceRepository sourceRepository, IMapper mapper)
+        {
+            this.articleRepository = articleRepository;
+            this.sourceRepository = sourceRepository;
+            this.mapper = mapper;
+
+        }
+
+        [Route("/editorsarticles/create")]
+        [HttpGet]
+        [Authorize]
+        public async Task<ActionResult<CreateDraftArticleOutputModel>> Create(CreateDraftArticleOutputModel model)
+        {
+            if (model.Errors.Count == 0)
+            {
+                return View(await CreateDraftArticleOutputModel.CreateAsync(articleRepository, sourceRepository));
+            }
+            model.Categories = await articleRepository.GetMenuCategories();
+            model.Sources = await sourceRepository.GetSources();
+            return View(model);
+        }
+
+        [Route("/editorsarticles/create")]
+        [HttpPost]
+        [Authorize]
+        public async Task<ActionResult<CreateArticleOutputModel>> Create(CreateArticleCommand command)
+        {
+            try
+            {
+                var result = await this.Send(command);
+                return result;
+            }
+            catch (ModelValidationException ex)
+            {
+                command.Errors = ex.Errors;
+                return RedirectToAction("Create", command);
+            }
+
+        }
+
+        [Route("/editorsarticles/edit")]
+        [HttpGet]
+        [Authorize]
+        public async Task<ActionResult<EditArticleOutputModel>> Edit([FromQuery] EditArticleOutputModelQuery query)
+        {
+            try
+            {
+                var model = await this.Mediator.Send(query);
+                model.Categories = await articleRepository.GetMenuCategories();
+                model.Sources = await sourceRepository.GetSources();
+                return View(model);
+            }
+            catch (NullReferenceException)
+            {
+                return Redirect("/Home/NotFound");
+            }
+        }
+
+        [Route("/editorsarticles/edit")]
+        [HttpPost]
+        [Authorize]
+        public async Task<ActionResult<EditArticleOutputModel>> Edit(EditArticleCommand command)
+        {
+            try
+            {
+                var model = await this.Mediator.Send(command);
+                return model;
+            }
+            catch (ModelValidationException ex)
+            {
+                return RedirectToAction("Edit", new EditArticleOutputModelQuery() { Id = command.Id, Errors = ex.Errors });
+            }
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
